de93ba74840fb8c23f05b9746a1704caca9a9f88
George Lifchits <george.lifchits@gmail.com>
2013-11-15 18:26:12 -0500

    added /profile to refactor

---
+++ app/profile.py
@@ -0,0 +1,59 @@
+
+from flask import render_template, request, url_for, redirect, flash, session
+from flask import Blueprint, current_app as app
+from schema import *
+from utils import *
+
+profile = Blueprint('profile', __name__)
+
+
+@profile.route('/profile')
+def edit_profile():
+    ''' Allow someone to view and edit their own profile. '''
+    if 'user' in session:
+        grab_photo(session['user'])
+        return render_template('profile.html', user=session['user'])
+    else:
+        flash((CSS_ERR, "You have to be logged in to view your profile!"))
+        return redirect(url_for('login'))
+
+
+@profile.route('/profile/save_changes', methods=['POST'])
+def save_profile():
+    ''' Accepts a save profile POST request. '''
+    form = request.form
+    app.logger.debug(form)
+    attribs = {
+        'name': 'profile-name',
+        'facebook': 'profile-facebook',
+        'phone': 'profile-phone',
+        'email': 'profile-email'
+    }
+    if 'user' in session:
+        user = session['user']
+        app.logger.debug('modifying' + str(user))
+
+    for attr in attribs.keys():
+        user[attr] = form[attribs[attr]]
+
+    return '200'
+
+
+@profile.route('/profile/<user_id>')
+def view_profile(user_id):
+    ''' Renders an individual user profile. '''
+    match = Driver.objects(id = user_id)
+    try:
+        assert match
+    except AssertionError:
+        flash((CSS_ERR, "Invalid profile ID"))
+        return redirect(url_for('home'))
+    app.logger.debug(match)
+    if match.count() != 1:
+        flash((CSS_ERR, "No user with that profile ID was found."))
+        return redirect(url_for('home'))
+    grab_photo(match[0])
+    return render_template('profile.html', user=match[0])
+
+
+
