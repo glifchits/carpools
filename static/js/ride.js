aea4296da0f0579004e4082cb1a667dfeee77ded
George Lifchits <george.lifchits@gmail.com>
2013-11-17 15:36:59 -0500

    refactored and send email WIP

--- static/js/ride.js
+++ static/js/ride.js
@@ -1,18 +1,38 @@
-$(function() {
+var sendEmail = function() {
+    console.log('sending email');
+    var params = {
+        'from'      : 'fromaddr',
+        'to'        : 'toaddr',
+        'subject'   : 'subject',
+        'message'   : 'message'
+    };
+    console.log(params);
+    $.post('/email', params);
+};
 
+// show send button only when the textarea is non empty
+var hideSend = function(hide) {
+    if (hide) {
+        $('#send-message').addClass('disabled');
+    } else {
+        $('#send-message').removeClass('disabled');
+    };
+};
+
+$(function() {
     // textarea autoresize
     $('#message').autosize();
 
-    // show send button only when the textare is non empty
-    var hideSend = function() {
-        if ($('#message').val() === '') {
-            $('#send-message').addClass('disabled');
-        } else {
-            $('#send-message').removeClass('disabled');
-        };
-    };
     $('#send-message').addClass('disabled');
+    $('#send-message').click(function() {
+        event.preventDefault();
+        if ($(this).hasClass('disabled')) {
+            console.log('disabled');
+        } else {
+            sendEmail();
+        };
+    });
     $('#message').keyup(function() {
-        hideSend();
+        hideSend($(this).val() === '');
     })
 });
