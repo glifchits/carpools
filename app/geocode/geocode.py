d0681a54976ba9b038a44b71add99c958a230a15
George Lifchits <george.lifchits@gmail.com>
2013-11-21 18:44:27 -0500

    geocode fixup

--- app/geocode/geocode.py
+++ app/geocode/geocode.py
@@ -1,5 +1,5 @@
 
-from config import CONFIG
+from google_api_config import CONFIG
 from geopy.geocoders import GoogleV3
 import requests
 import urllib
@@ -16,36 +16,48 @@ class Geocoder(object):
         address, lat, lon = self.google.geocode(string)
         return (lat, lon)
 
-    def nearby_search(self, lat, lon, radius=10000, rankby='distance',
+    def nearby_search(self, lat, lon, radius=10000, rankby='prominence',
                       sensor='true', types='establishment', **kwargs):
         ''' Return the results of a place search nearby `lat` and `lon` '''
         url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
         params = {
             'key' : CONFIG.api_key,
             'location' : '%s,%s' % (lat, lon),
-            'radius' : radius,
             'rankby' : rankby,
             'sensor' : sensor,
             'types'  : types
-        }.update(dict(**kwargs))
+        }
+        if params['rankby'] != 'distance':
+            params['radius'] = radius
+        params.update(dict(**kwargs))
         url = url + urllib.urlencode(params)
-        return requests.get(url).json()
+        r = requests.get(url)
+        try:
+            return r.json()
+        except:
+            return r.text
 
-    def text_search(self, query, sensor='true', **kwargs):
+    def text_search(self, query, lat, lon, sensor='true', **kwargs):
         ''' Perform a text search with `query` '''
         url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
         params = {
             'key': CONFIG.api_key,
             'query': query,
-            'sensor' : sensor
-        }.update(dict(**kwargs))
+            'sensor' : sensor,
+            'location': '%s,%s' % (lat, lon)
+        }
+        params.update(dict(**kwargs))
         url = url + urllib.urlencode(params)
-        return requests.get(url).json()
+        r = requests.get(url)
+        try:
+            return r.json()
+        except:
+            return r.text
 
     def radar_search(self, lat, lon, sensor='true', radius=10000,
                      types='establishment', **kwargs):
         ''' Perform a radar search with `keyword`, `name`, or `types` '''
-        url = 'https://maps.googleapis.com/maps/api/place/radarsearch/output?'
+        url = 'https://maps.googleapis.com/maps/api/place/radarsearch/json?'
         params = {
             'key' : CONFIG.api_key,
             'location': '%s,%s' % (lat, lon),
@@ -54,8 +66,12 @@ class Geocoder(object):
             'types'  : types
         }
         params.update(dict(**kwargs))
-        print params
         url = url + urllib.urlencode(params)
-        return requests.get(url).json()
+        r = requests.get(url)
+        try:
+            return r.json()
+        except:
+            return r.text
+
 
 
