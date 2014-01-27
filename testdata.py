0b171a28ef586c32211d05634ff64578cb70b137
George Lifchits <george.lifchits@gmail.com>
2014-01-26 21:27:20 -0500

    updated testdata script

--- testdata.py
+++ testdata.py
@@ -3,6 +3,8 @@ import random
 import urllib
 import json
 import requests
+import os
+import sys
 
 from mongoengine import *
 from app.schema import *
@@ -32,76 +34,84 @@ EMAILS = set([
     "gmail.com", "hotmail.com", "yahoo.com", "mail.ru"
 ])
 
-for i in range(20):
-    name = random.choice(list(NAMES))
-    email = name.lower() + '%s@' + random.choice(list(EMAILS))
-    password = 'password'
-    email_salt = ''
+SKIP_USERS = '--no-users' in sys.argv
+SKIP_RIDES = '--no-rides' in sys.argv
 
-    found = False
-    while not found:
-        fb_id = random.randint(1000, 50000)
-        res = urllib.urlopen('http://graph.facebook.com/%s' % fb_id).read()
-        res = json.loads(res)
-        found = 'error' not in res.keys()
+if not SKIP_USERS:
+    if not os.path.exists('static/temp'):
+        os.mkdir('static/temp')
 
-    found = False
-    while not found:
-        email_salt = random.randint(0, 100)
-        found = Driver.objects(email = email % email_salt).count() == 0
+    for i in range(20):
+        name = random.choice(list(NAMES))
+        email = name.lower() + '%s@' + random.choice(list(EMAILS))
+        password = 'password'
+        email_salt = ''
 
-    facebook = Facebook(
-        user_id = fb_id,
-        access_token = "1z2x3c4v5b6n7m",
-        expires_at = int(time.time())
-    ).save()
-    print "created %s" % facebook
+        found = False
+        while not found:
+            fb_id = random.randint(1000, 50000)
+            res = urllib.urlopen('http://graph.facebook.com/%s' % fb_id).read()
+            res = json.loads(res)
+            found = 'error' not in res.keys()
 
-    driver = Driver(
-        name = name,
-        email = email % email_salt,
-        password = password,
-        facebook = facebook
-    )
-    img_url = 'http://graph.facebook.com/%s/picture?width=300&height=300'
-    image_request = requests.get(img_url % fb_id, stream=True)
-    if image_request.status_code == 200:
-        image_path = 'static/temp/profile_temp.jpg'
-        image = open(image_path, 'wb')
-        for chunk in image_request.iter_content():
-            image.write(chunk)
-        image.close()
-        image = open(image_path, 'r')
-        driver.photo.put(image, content_type='image/jpeg')
-        image.close()
-    driver.save()
-    print "created %s" % driver
+        found = False
+        while not found:
+            email_salt = random.randint(0, 100)
+            found = Driver.objects(email = email % email_salt).count() == 0
 
-print """
-Creating rides
-"""
+        facebook = Facebook(
+            user_id = fb_id,
+            access_token = "1z2x3c4v5b6n7m",
+            expires_at = int(time.time())
+        ).save()
+        print "created %s" % facebook
 
-for i in range(100):
-    dep = random.choice(list(CITIES))
+        driver = Driver(
+            name = name,
+            email = email % email_salt,
+            password = password,
+            facebook = facebook
+        )
+        img_url = 'http://graph.facebook.com/%s/picture?width=300&height=300'
+        image_request = requests.get(img_url % fb_id, stream=True)
+        if image_request.status_code == 200:
+            image_path = 'static/temp/profile_temp.jpg'
+            image = open(image_path, 'wb')
+            for chunk in image_request.iter_content():
+                image.write(chunk)
+            image.close()
+            image = open(image_path, 'r')
+            driver.photo.put(image, content_type='image/jpeg')
+            image.close()
+        driver.save()
+        print "created %s" % driver
 
-    now = datetime.now()
-    timeoffset = timedelta(
-        days = random.randint(-5, 40),
-        hours = random.randint(-4, 10),
-        minutes = random.randint(0, 60),
-        seconds = random.randint(0, 60)
-    )
-    depart_time = now + timeoffset
+if not SKIP_RIDES:
+    print """
+    Creating rides
+    """
 
-    driver = Driver.objects[random.randint(0, Driver.objects.count()-1)]
-    ride = Ride(
-        driver = driver,
-        departure = dep,
-        destination = random.choice(list(CITIES - set(dep))),
-        depart_date = depart_time,
-        people = random.randint(2,6)
-    )
-    ride.save()
-    print "created %s" % ride
+    for i in range(100):
+        dep = random.choice(list(CITIES))
+
+        now = datetime.now()
+        timeoffset = timedelta(
+            days = random.randint(-5, 40),
+            hours = random.randint(-4, 10),
+            minutes = random.randint(0, 60),
+            seconds = random.randint(0, 60)
+        )
+        depart_time = now + timeoffset
+
+        driver = Driver.objects[random.randint(0, Driver.objects.count()-1)]
+
+        ride = Ride(
+            driver = driver,
+            depart_date = depart_time,
+            people = random.randint(2,6)
+        )
+        ride.set_places(dep, random.choice(list(CITIES - set(dep))))
+        ride.save()
+        print "created %s" % ride
 
 
